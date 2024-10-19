3c4417adcbdbed72fb335131e5996b999196181b
Alkanaft17 <alekseyka1700@gmail.com>
2024-10-19 17:26:16 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -1,5 +1,4 @@
 print('Введите три числа: ')
-#num1, num2, num3 = map(int, input().split())
 num1, num2, num3 = int(input('Первое число: ')), int(input('Второе число: ')), int(input('Третье число: '))
 if num1 == num2 == num3:
     print(3)
